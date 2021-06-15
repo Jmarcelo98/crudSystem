f24b25373dbba7d0c80561fc7b736f081e6a1a75
Joao Marcelo <joaomarcelo588@gmail.com>
2021-06-15 13:07:07 -0300

    Fix(auto import sql)

--- back-end/src/main/resources/import.sql
+++ back-end/src/main/resources/import.sql
@@ -3,4 +3,4 @@ insert into login (id, password, username, type) values  (null, '123456', 'comum
 
 insert into client (id, nome, cpf, cep, logradouro, bairro, cidade, uf, complemento, email, telefones) values (null, 'JOAO MARCELO', '05455329146', '73251902', 'CONDOMINIO PARQUE COLORADO', 'SOBRADINHO', 'BRASILIA', 'DF', null, 'JOAOMARCELO588@GMAIL.COM', '61994322941');
 
-insert into client (id, nome, cpf, cep, logradouro, bairro, cidade, uf, complemento, email, telefones) values (null, 'LUCAS', '04539245684', '71551208', 'SHTQ QUADRA 2 CONJUNTO 2', 'LAGO NORTE', 'BRASILIA', 'DF', null, 'LUCAS@GMAIL.COM', '619999999');
+insert into client (id, nome, cpf, cep, logradouro, bairro, cidade, uf, complemento, email, telefones) values (null, 'LUCAS', '04539245684', '71551208', 'SHTQ QUADRA 2 CONJUNTO 2', 'LAGO NORTE', 'BRASILIA', 'DF', null, 'LUCAS@GMAIL.COM', '61999999999');
